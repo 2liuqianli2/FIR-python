2a21b793d854517f3a1c9139aea6a7e2f21d5e5a
2liuqianli2 <3047708553@qq.com>
2023-03-11 23:50:10 +0800

    3.11-23：50

---
+++ constant.py
@@ -0,0 +1,46 @@
+
+# 总大小
+board_x = 740
+board_y = 500
+
+# 棋盘起始位置
+local_xy = 31
+
+
+# 小格子大小
+size_box = 25
+
+# 棋盘大小
+big_box = 450
+
+# 线条数量
+line_number = 19
+
+# b_color = (223,223,223)
+# 线条颜色
+line_color = (0,0,0)
+sline_color = (70,70,70)
+
+#横向标记起始位置
+linex_x = 11
+linex_y = 25
+
+# 纵向标记起始位置
+liney_y = 15
+liney_x = 28
+# 棋子颜色
+w_qicolor = (245,245,245)
+b_qicolor = (30,30,30)
+# 棋子半径
+qi_radius = int(size_box/2-2)
+
+q_finish=1
+q_win = 2
+q_err= -1
+q_pos_placed= -4
+q_stat_err = -2
+q_ok= 0
+q_range_err = -3
+
+panel_x=[418,740]
+panel_y=[0,500]
